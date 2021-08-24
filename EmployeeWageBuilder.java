9984fde0898266fd8fdb3d6d7c481ba97cf43fad
sonam1812 <sonampal832@gmail.com>
2021-08-24 10:17:01 +0530

    Added

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,5 +3,6 @@ package com.EmployeeWage;
 public class EmployeeWageBuilder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee wage");
+	System.out.println("this is empwage");
 }
 }
