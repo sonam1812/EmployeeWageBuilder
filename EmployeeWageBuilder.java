b017645ee5bac3de6a956149f3977fa534e88df4
sonam1812 <sonampal832@gmail.com>
2021-08-25 15:37:52 +0530

    Added uc2-file

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,13 +2,18 @@ package com.EmployeeWage;
 
 public class EmployeeWageBuilder {
 public static void main(String[] args) {
-	int IS_FULL_TIME =1;
+	int IS_FULL_TIME = 1;
+	int EMP_RATE_PER_HOUR = 20;
+	int empHrs = 0;
+	int empWage = 0;
 	double empCheck = (Math.floor(Math.random() * 10) % 2);
-	System.out.println(empCheck);
-	if (empCheck == IS_FULL_TIME) {
-	System.out.println("employee is present");
-	} else {
-	System.out.println("employee is absent");
-}
+	if (empCheck == IS_FULL_TIME)
+		empHrs = 8;
+	else
+		empHrs = 0;
+	
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Emp Wage: " + empWage);
+	
 }
 }
