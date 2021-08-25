7f18322e6d677372a276727092d157b496e74e35
sonam1812 <sonampal832@gmail.com>
2021-08-25 14:57:06 +0530

    Added uc1 file

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,7 +2,13 @@ package com.EmployeeWage;
 
 public class EmployeeWageBuilder {
 public static void main(String[] args) {
-	System.out.println("Welcome to Employee wage");
-	System.out.println("this is empwage");
+	int IS_FULL_TIME =1;
+	double empCheck = (Math.floor(Math.random() * 10) % 2);
+	System.out.println(empCheck);
+	if (empCheck == IS_FULL_TIME) {
+	System.out.println("employee is present");
+	} else {
+	System.out.println("employee is absent");
+}
 }
 }
